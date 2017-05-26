d3b26659352c90743a0ff035c408588286b7e435
Joey C <joeyc@Joeys-iMac.local>
2017-05-25 22:17:18 -0400

    fixed propTypes

--- components/List/index.js
+++ components/List/index.js
@@ -3,17 +3,16 @@ import {StyleSheet, View, Text} from 'react-native'
 import PropTypes from 'prop-types'
 
 export default List = props => (
-    static propTypes = {
-        props.title = PropTypes.string.isRequired,
-        props.cards = PropTypes.array.isRequired,
-    }
-
     <View style = {styles.list}>
         <Text>{this.props.title}</Text>
         {this.props.cards.map(card => <Card key={card.id} {...card} />)}
     </View>
 )
 
+List.propTypes = {
+    props.title = PropTypes.string.isRequired,
+    props.cards = PropTypes.array.isRequired,
+}
 
 const styles = StyleSheet.create({
     list: {
