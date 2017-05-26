a85fb400a1fedba2f4391540cb52c4efcc6b7cb6
Joey C <joeyc@Joeys-iMac.local>
2017-05-25 21:33:04 -0400

    fixed typo

--- components/List/index.js
+++ components/List/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import {StyleSheet, View} from 'react-native'
 
-export default class List extends Componet {
+export default class List extends Component {
     render() {
         return (
             <View style = styles.list>
