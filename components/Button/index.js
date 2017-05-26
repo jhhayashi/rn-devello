f487f950a48b8cb21c29f427f976881e47a3ce79
Galen Lee <glee@Galens-MacBook-Pro.local>
2017-05-26 10:44:08 -0400

    created button

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,66 +1,51 @@
 import React, { Component } from 'react'
-import {StyleSheet, View} from 'react-native'
+import {StyleSheet, View, TouchableOpacity, Text} from 'react-native'
 import PropTypes from 'prop-types'
 
 export default class Button extends Component {
     static propTypes = {
         text: PropTypes.string,
         onClick: PropTypes.func.isRequired,
+        variant: PropTypes.oneOf(['forward', 'backward', 'delete', 'close', 'default'])
     }
 
     static defaultProps = {
         text: 'Click Me!',
     }
-
-    buttonColor = (type) => {
-        return {
-            if(type === 'default') return {backgroundColor: '#898989'}
-            else if(type === 'forward') return {backgroundColor: '#34ff1a'}
-            else if(type === 'backward') return {backgroundColor: 'green'}
-            else if(type === 'delete') return {backgroundColor: '#ff0300'}
-            else return {backgroundColor: '#34ff1a'}
-        }
-    }
-
     render() {
-        if (this.props.text === 'default')
         return (
-            <View style={styles.container}>
-                <TouchableOpacity onPress={this.props.onClick} style={[styles.button, buttonColor(this.props.text)]}>
-                    {this.props.text}
-                </TouchableOpacity>
-            </View>
+            <TouchableOpacity onPress={this.props.onClick} style={[styles.button, styles[this.props.variant]]}>
+                <Text style={styles.text}>{this.props.text}</Text>
+            </TouchableOpacity>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingTop: Constants.statusBarHeight,
-        backgroundColor: '#ecf0f1',
-    },
     button: {
         padding: 5,
         borderRadius: 5,
+        backgroundColor: '#ecf0f1',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
         fontSize: 10,
         color: '#ffffff',
     },
-    // default: {
-    //     backgroundColor: '#898989',
-    // },
-    // forward: {
-    //     backgroundColor: '#34ff1a',
-    // },
-    // backward: {
-    //     backgroundColor: 'green',
-    // },
-    // delete: {
-    //     backgroundColor: '#ff0300',
-    // },
-    // create {
-    //     backgroundColor: '#34ff1a',
-    // },
+    default: {
+        backgroundColor: '#898989',
+    },
+    forward: {
+        backgroundColor: '#34ff1a',
+    },
+    backward: {
+        backgroundColor: 'green',
+    },
+    delete: {
+        backgroundColor: '#ff0300',
+    },
+    create: {
+        backgroundColor: '#34ff1a',
+    },
 })
--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from './components/Button';
 
 class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up main.js to start working on your app!</Text>
+        <Button text='delete' onClick={() => console.log('hello')} variant='delete'/>
       </View>
     );
   }
